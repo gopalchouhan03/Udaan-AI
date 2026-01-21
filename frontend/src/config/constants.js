789176e8522805625e5da48ac92bd24fd1cd2678
gopal <gkrajput9450@gmail.com>
2026-01-21 13:22:39 +0530

    constant base url change

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,5 +1,5 @@
 // src/config/constants.js
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
 export const ROUTES = {
   HOME: '/',
   LOGIN: '/login',
